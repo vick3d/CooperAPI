acf062b43ec7e1295291a1d01ba0a0605b8b651b
vick3d <viktoro0090@gmail.com>
2019-03-06 10:01:03 +0100

    adds devise_token_auth gem and added specs for user registration and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
-    end
-  end
+		end
+
+		namespace :v1 do
+			mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+		end
+	end
 end
